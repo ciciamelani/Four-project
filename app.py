fc733d0f92a711e8bacefe674ff1fc7bd359f808
ciciamelani <112041504+ciciamelani@users.noreply.github.com>
2022-08-27 20:26:21 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+# Menginput alas dan tinggi segitiga
+alas = float(input('tulis alas segitiga: '))
+tinggi = float(input('tulis tinggi segitiga: '))
+
+# Hitung luas segitiga
+luas = (alas * tinggi ) / 2
+
+# Menampilkan hasil perhitungan
+print('luas segitiga adalah %0.2f'%luas)
